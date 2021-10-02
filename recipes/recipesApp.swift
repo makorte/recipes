c5969c192b2dbf3f079cd38a1ee532b4f07119d1
Max Korte <privat@max-krote.de>
2021-10-02 18:22:49 +0200

    Initial Commit

---
+++ recipes/recipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipesApp.swift
+//  recipes
+//
+//  Created by Max Korte on 02.10.21.
+//
+
+import SwiftUI
+
+@main
+struct recipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
